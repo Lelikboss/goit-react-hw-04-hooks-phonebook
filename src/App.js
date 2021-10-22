9cd2f7c35648e268ee7ccb64b474cf364d3b0e60
Alexey Alexeew <lelikboss97@gmail.com>
2021-10-22 11:17:51 +0300

    add useLS

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
+import { useLS } from './hooks/useLS';
 import ContactForm from './Components/ContactForm';
 import ContactList from './Components/ContactList';
 import ContactItem from './Components/ContactItem';
 import Filter from './Components/Filter';
 import MainContainer from './Components/MainContainer';
 export default function App() {
-  const [contacts, setContacts] = useState(() => {
-    return (
-      JSON.parse(localStorage.getItem('contacts')) ?? [
-        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-      ]
-    );
-  });
-
-  useEffect(() => {
-    window.localStorage.setItem('contacts', JSON.stringify(contacts));
-  }, [contacts]);
-
+  const [contacts, setContacts] = useLS('contacts', [
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ]);
   const [filter, setFilter] = useState('');
   const addNewContact = obj => {
     const repeatingName = contacts
